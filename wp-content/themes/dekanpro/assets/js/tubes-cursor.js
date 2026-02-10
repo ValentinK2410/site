261291cc0ae894b12cb10c6c58ea49a2a08748ef
Valentink2410 <valentink2410@gmail.com>
2026-02-10 23:59:05 +0400

    Tubes Cursor: линии теперь позади контента (z-index: -1)

--- wp-content/themes/dekanpro/assets/js/tubes-cursor.js
+++ wp-content/themes/dekanpro/assets/js/tubes-cursor.js
@@ -57,7 +57,7 @@
                 width: 100%;
                 height: 100%;
                 pointer-events: none;
-                z-index: 9999;
+                z-index: -1;
                 opacity: 1;
             `;
             document.body.appendChild(this.canvas);
